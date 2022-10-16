82bb41497a3c5c8225a023efdf0e6cae71974f95
Greg O'Lone <greg@stretchedout.com>
2022-10-15 23:24:04 -0400

    Add the jwtsign go binary and source to the project

--- Build Automation.xojo_code
+++ Build Automation.xojo_code
@@ -6,6 +6,14 @@
 			Begin BuildStepList Mac OS X
 				Begin BuildProjectStep Build
 				End
+				Begin CopyFilesBuildStep CopyBundleContents
+					AppliesTo = 0
+					Architecture = 0
+					Target = 0
+					Destination = 4
+					Subdirectory = 
+					FolderItem = Li4vX0Fzc2V0cy9IZWxwZXJzLw==
+				End
 				Begin SignProjectStep Sign
 				  DeveloperID=
 				End
---
+++ jwt/signature/signature.go
@@ -0,0 +1,72 @@
+package signature
+
+import (
+	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"io/ioutil"
+	"math/big"
+	"strings"
+)
+
+type EcdsaSignature struct {
+	R, S *big.Int
+}
+
+func Sign(header string, payload string, keyfile string) (string, error) {
+	// Sign with SHA256 hash algorithms
+	h := sha256.New()
+	h.Write([]byte(header + "." + payload))
+	hash := h.Sum(nil)
+	// Read private key content from file
+	
+	data, err := ioutil.ReadFile(keyfile)
+	if nil != err {
+		return "", err
+	}
+	// The .p8 is a PKCS#8 format's pem(The base64 format of DER(Encoded ASN.1 structure)) file
+	// ASN.1	- Abstract Syntax Notation One
+	// BER		- Basic Encoding Rules
+	// DER		- Distinguished Encoding Rules
+	block, _ := pem.Decode(data)
+	priv, err := x509.ParsePKCS8PrivateKey(block.Bytes) // Parse an unencrypted, PKCS#8 private key
+	if nil != err {
+		return "", err
+	}
+
+	// Do Sign
+	ecdsaKey, _ := priv.(*ecdsa.PrivateKey)
+	if r, s, err := ecdsa.Sign(rand.Reader, ecdsaKey, hash); err == nil {
+
+		curveBits := ecdsaKey.Curve.Params().BitSize
+
+		keyBytes := curveBits / 8
+		if curveBits%8 > 0 {
+			keyBytes += 1
+		}
+
+		// We serialize the outpus (r and s) into big-endian byte arrays and pad
+		// them with zeros on the left to make sure the sizes work out. Both arrays
+		// must be keyBytes long, and the output must be 2*keyBytes long.
+		rBytes := r.Bytes()
+		rBytesPadded := make([]byte, keyBytes)
+		copy(rBytesPadded[keyBytes-len(rBytes):], rBytes)
+
+		sBytes := s.Bytes()
+		sBytesPadded := make([]byte, keyBytes)
+		copy(sBytesPadded[keyBytes-len(sBytes):], sBytes)
+
+		out := append(rBytesPadded, sBytesPadded...)
+
+		return strings.TrimRight(base64.URLEncoding.EncodeToString(out), "="), nil
+
+	} else {
+
+		return "", err
+
+	}
+
+}
